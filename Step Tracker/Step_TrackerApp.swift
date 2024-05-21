1c011517b0a70d143d6fd20107daa6c3cc8c4d46
jeremykoo <38170465+jeremykoo@users.noreply.github.com>
2024-05-21 15:04:26 -0700

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jeremy Koo on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
