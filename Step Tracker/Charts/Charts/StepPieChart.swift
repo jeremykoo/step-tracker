21ffd26ab5ee31ba2683fcbc591c267bd070958e
jeremykoo <38170465+jeremykoo@users.noreply.github.com>
2024-08-03 22:18:43 -0700

    first accessibility pass

--- Step Tracker/Charts/Charts/StepPieChart.swift
+++ Step Tracker/Charts/Charts/StepPieChart.swift
@@ -39,6 +39,8 @@ struct StepPieChart: View {
                     .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
                     .opacity(selectedWeekDay?.date.weekdayInt == weekday.date.weekdayInt ? 1.0 : 0.3)
+                    .accessibilityLabel(weekday.date.weekdayTitle)
+                    .accessibilityValue("\(Int(weekday.value)) steps")
                 }
             }
             .chartAngleSelection(value: $rawSelectedChartValue)
@@ -70,6 +72,7 @@ struct StepPieChart: View {
                                     .contentTransition(.numericText())
                             }
                             .position(x: frame.midX, y: frame.midY)
+                            .accessibilityHidden(true)
                         }
                     }
                 }
